9749a9139136c536c93b774867f305e370f2cbcc
82huhu <peanutpenguin88@gmail.com>
2024-12-09 21:55:56 -0500

    Delete Constants.java

--- Constants.java
+++
@@ -1,8 +0,0 @@
-package sketchy.main;
-
-public class Constants {
-    public static final int SCENE_WIDTH = 1200;
-    public static final int SCENE_HEIGHT = 600;
-    public static final int CONTROL_WIDTH = SCENE_WIDTH / 8;
-    public static final int CONTROL_SPACING = 6;
-}
